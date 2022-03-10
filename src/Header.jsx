b51643e6d34c0ed2df0e83ee01f7cb62ba99c7a3
Rajat Guptat <rajatgupta.tech@gmail.com>
2022-03-10 18:07:21 +0530

    added styling

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='heading'>Krish-e-हल</div>
+  )
+}
---
+++ src/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PostCards from './PostCards';
+
+function Posts() {
+      // let [posts, setPosts] = useState([]); //for showing all posts in Descending order of date
+
+      // //create set posts from firebase
+
+      // posts.map((product, index) => {
+      //   return(<div key={index}> <PostCards product={product} index={index} /> </div>);
+      // })
+}
+
+export default Posts;
